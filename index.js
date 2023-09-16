05251f102e8159174ecce84a5b816add83f3517d
RaphaelPursino <107816621+RaphaelPursino@users.noreply.github.com>
2023-09-16 10:54:47 -0300

    Add files via upload
    atualização

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
+
+const boxes = document.querySelectorAll('.box')
+
+const checkboxes = () => {
+    const triggerBottom = (window.innerHeight / 5) * 4
+    boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top
+        if (boxTop < triggerBottom) box.classList.add("show")
+        else box.classList.remove("show")
+    })
+}
+
+window.addEventListener('scroll', checkboxes)
+checkboxes()
